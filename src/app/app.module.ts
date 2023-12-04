680d6fc1c44c1f0161d51a149944d7d704b9749c
CharlesTravail <charles.chevreuil@gmail.com>
2023-12-04 14:55:30 -0500

    des trucs plein de truc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './component/main/main.component';
+import { CardComponent } from './component/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/component/card/card.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss']
+})
+export class CardComponent {
+  @Input() nomBiere:string;
+  @Input() type:string;
+  @Input() pay:string;
+  @Input() photo:string;
+}
