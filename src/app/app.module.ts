600d8988de04947b286875e0c01e54925854d22c
charlesIGN <charles.chevreuil@gmail.com>
2023-12-22 17:33:36 -0500

    ajout prodier pour le httpModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, importProvidersFrom } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -28,7 +28,7 @@ import { HttpClientModule } from '@angular/common/http';
     AppRoutingModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [importProvidersFrom(HttpClientModule)],
   bootstrap: [ConfrerieComponent]
 })
 export class AppModule { }
