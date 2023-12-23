9ddd6145a4162c41a7558a4bdb62f4870048399d
charlesIGN <charles.chevreuil@gmail.com>
2023-12-22 21:58:14 -0500

    router et httpclent

--- src/app/component/lesbieres/lesbieres.component.spec.ts
+++ src/app/component/lesbieres/lesbieres.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LesbieresComponent } from './lesbieres.component';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('LesbieresComponent', () => {
   let component: LesbieresComponent;
@@ -8,7 +9,8 @@ describe('LesbieresComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LesbieresComponent ]
+      declarations: [ LesbieresComponent ],
+      imports:[HttpClientModule]
     })
     .compileComponents();
 
