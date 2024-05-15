cf4b75ced022472c7f1a366182fc082764e86893
charlesIGN <charles.chevreuil@gmail.com>
2024-05-15 11:30:14 +0200

    environement

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
-   baseUrl:'http://localhost:8080/',
+   baseUrl:'http://51.222.9.109:8082/',
+   //baseUrl:'http://localhost:8080/',
    pass:'Q29uZnJlcmU6TGFiaWVyZTc4Niw='
 }
