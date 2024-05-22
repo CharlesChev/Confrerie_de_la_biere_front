2bd83bd3c5033e736e528f3a9e2e3e47cb4e90d1
CharlesChev <65682464+CharlesChev@users.noreply.github.com>
2024-05-22 16:43:48 +0200

    Update environment.ts

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,7 +1,5 @@
 export const environment = {
    baseUrl:'http://localhost:8082/',
    //baseUrl:'http://51.222.9.109:8082/',
-   pass:'Q29uZnJlcmU6YmVib3A=',
-   //pass:'Q29uZnJlcmU6TGFiaWVyZTc4Niw=',
    pathToImagesFolder:'assets/'
 }
