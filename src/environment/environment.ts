fc662e8e3a590d3fc28b0cd61ad65e93dd864696
charlesIGN <charles.chevreuil@gmail.com>
2024-05-22 16:27:17 +0200

    ajustement css

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,5 +1,7 @@
 export const environment = {
    baseUrl:'http://localhost:8082/',
-   pass:"",
+   //baseUrl:'http://51.222.9.109:8082/',
+   pass:'Q29uZnJlcmU6YmVib3A=',
+   //pass:'Q29uZnJlcmU6TGFiaWVyZTc4Niw=',
    pathToImagesFolder:'assets/'
 }
